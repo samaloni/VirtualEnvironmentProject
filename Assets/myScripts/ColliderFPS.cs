2fe2848c27a0ba196c7f8a8dd094f12665eabde3
saloni <aloni.s@husky.neu.edu>
2017-08-11 21:06:55 -0400

    First draft

---
+++ Assets/myScripts/ColliderFPS.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ColliderFPS : MonoBehaviour {
+
+    public GameObject keyScoreUI;
+    public static int key = 0;
+    public AudioSource audiosrc;
+    public AudioClip beepClip;
+
+
+     void Start()
+    {
+        keyScoreUI = GameObject.Find("KeyScore");
+       // audiosrc = GetComponent<AudioSource>();
+       // audiosrc.clip = beepClip;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+       Debug.Log(gameObject.name +" Collided with " +collision.collider.name );
+       if(collision.collider.name == "FPSController")
+        {
+           
+         //   audiosrc.Play();
+            key++;
+            Debug.Log("Destroying the game object. Num keys = " +key);
+            Destroy(gameObject);
+            keyScoreUI.GetComponent<Text>().text = "Key: " + key;
+        }
+
+    }
+
+
+}
---
+++ Assets/myScripts/CollisionDetection.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+function OnCollisionEnter (c : Collision)
+{
+	print("collided with " + c.gameObject.name);
+	
+
+}
